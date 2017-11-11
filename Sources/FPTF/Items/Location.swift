ed0777c57ed27140ce5bc81a9c0d0d43dbf60637
Kilian Koeltzsch <me@kilian.io>
2017-11-11 22:28:53 +0100

    reorder location name and address fields

--- Sources/FPTF/Items/Location.swift
+++ Sources/FPTF/Items/Location.swift
@@ -1,15 +1,16 @@
 public struct Location: Item, Codable {
     private let type = "location"
-    public let address: String?
+
     public let name: String?
+    public let address: String?
 
     public let longitude: Double?
     public let latitude: Double?
     public let altitude: Double?
 
-    public init(address: String?, name: String?, longitude: Double?, latitude: Double?, altitude: Double?) {
-        self.address = address
+    public init(name: String?, address: String?, longitude: Double?, latitude: Double?, altitude: Double?) {
         self.name = name
+        self.address = address
         self.longitude = longitude
         self.latitude = latitude
         self.altitude = altitude
@@ -18,8 +19,8 @@ public struct Location: Item, Codable {
 
 extension Location: Equatable {
     public static func ==(lhs: Location, rhs: Location) -> Bool {
-        return lhs.address == rhs.address &&
-            lhs.name == rhs.name &&
+        return lhs.name == rhs.name &&
+            lhs.address == rhs.address &&
             lhs.longitude == rhs.longitude &&
             lhs.latitude == rhs.latitude &&
             lhs.altitude == rhs.altitude
