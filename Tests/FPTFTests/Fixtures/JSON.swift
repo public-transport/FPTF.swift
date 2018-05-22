386199efbca8bb18c91faa4214021afe0b2bf1f8
Kilian Koeltzsch <me@kilian.io>
2018-05-22 22:49:49 +0200

    update json utility foo in test bundle

--- Tests/FPTFTests/Fixtures/JSON.swift
+++ Tests/FPTFTests/Fixtures/JSON.swift
@@ -2,7 +2,7 @@ import Foundation
 import FPTF
 
 enum JSON {
-    static func decode<T: Item>(json: String) throws -> T {
+    static func load(json: String) throws -> Data {
         let pwd: String
         if let xcode_pwd = ProcessInfo.processInfo.environment["XCODE_PWD"] {
             pwd = xcode_pwd
@@ -13,7 +13,11 @@ enum JSON {
         }
 
         let url = URL(fileURLWithPath: "\(pwd)/Tests/FPTFTests/Fixtures/\(json).json")
-        let data = try Data(contentsOf: url)
+        return try Data(contentsOf: url)
+    }
+
+    static func decode<T: Item>(json: String) throws -> T {
+        let data = try load(json: json)
 
         let decoder = JSONDecoder()
         if #available(OSX 10.12, *) {
@@ -21,6 +25,18 @@ enum JSON {
         }
 
         return try decoder.decode(T.self, from: data)
+    }
+
+    static func string(json: String) -> String? {
+        guard let data = try? load(json: json) else { return nil }
+        return String(data: data, encoding: .utf8)
+    }
+
+    static func encode<T: Item>(_ item: T) throws -> String {
+        let encoder = JSONEncoder()
+//        encoder.outputFormatting = .prettyPrinted
+        let data = try encoder.encode(item)
+        return String(data: data, encoding: .utf8)!
     }
 }
 
